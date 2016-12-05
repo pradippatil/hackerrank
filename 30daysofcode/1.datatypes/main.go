e19cc03a5b15d099275993a33b86c505332d86eb
Pradip Patil <pradippatil@users.noreply.github.com>
2016-12-04 23:09:57 -0500

    .

---
+++ 30daysofcode/1.datatypes/main.go
@@ -0,0 +1,26 @@
+//https://www.hackerrank.com/challenges/30-data-types
+
+package main
+
+import (
+	"bufio"
+	"fmt"
+	"os"
+)
+
+func main() {
+	var i uint32 = 4
+	var d float32 = 4.0
+	var s string = "HackerRank "
+	scanner := bufio.NewReader(os.Stdin)
+
+	var i2 uint32
+	var d2 float32
+	var s2 string
+	// Scan int and float
+	fmt.Scan(&i2)
+	fmt.Scan(&d2)
+	s2, _ = scanner.ReadString('\n')
+	fmt.Printf("%d\n%.1f\n%s%s\n", i+i2, d+d2, s, s2)
+
+}
