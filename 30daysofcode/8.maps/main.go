356b2c9d5196c87f5cc746f5750d363957a8d4b3
Pradip Patil <pradippatil@users.noreply.github.com>
2016-12-10 07:30:42 -0500

    .

--- 30daysofcode/8.maps/main.go
+++ 30daysofcode/8.maps/main.go
@@ -19,8 +19,8 @@ func main() {
 	if N < 1 && N >= int(math.Pow(10, 5)) {
 		return
 	}
+	scanner := bufio.NewScanner(os.Stdin)
 	for i := 0; i < N; i++ {
-		scanner := bufio.NewScanner(os.Stdin)
 		if ok := scanner.Scan(); ok {
 			input := strings.Split(scanner.Text(), " ")
 			phoneBook[input[0]] = input[1]
@@ -28,7 +28,6 @@ func main() {
 
 	}
 	var inputNames []string
-	scanner := bufio.NewScanner(os.Stdin)
 	for scanner.Scan() {
 		inputNames = append(inputNames, scanner.Text())
 	}
@@ -37,9 +36,6 @@ func main() {
 		return
 	}
 	for index := range inputNames {
-		fmt.Println(inputNames[index])
-
-		fmt.Println(phoneBook[inputNames[index]])
 		val, ok := phoneBook[inputNames[index]]
 		if ok {
 			fmt.Printf("%s=%s\n", inputNames[index], val)
